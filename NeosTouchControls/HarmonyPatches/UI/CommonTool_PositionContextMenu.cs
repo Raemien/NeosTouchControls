255295cfbf91ba6ec53332b18f2e1eed89c3c38b
Raemien <39755457+Raemien@users.noreply.github.com>
2023-07-15 13:37:37 +1200

    Rescale context menu for mobile screen sizes

---
+++ NeosTouchControls/HarmonyPatches/UI/CommonTool_PositionContextMenu.cs
@@ -0,0 +1,15 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+
+namespace NeosTouchControls.HarmonyPatches
+{
+    [HarmonyPatch("FrooxEngine.CommonTool+<PositionContextMenu>d__334", "MoveNext")]
+    internal class CommonTool_PositionContextMenu
+    {
+        public static void Postfix(ContextMenu ___menu)
+        {
+            Slot visual = ___menu.Slot;
+            visual.LocalScale *= 1.5f;
+        }
+    }
+}
